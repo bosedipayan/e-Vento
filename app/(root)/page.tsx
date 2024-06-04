a9436c43cb258065d24a8829dde968bdda5e61d1
bosedipayan <bosedipayan2000@gmail.com>
2024-06-05 02:32:29 +0530

    Done with footer and started creating homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,5 +2,15 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold"></h1>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
