77f4f535849056c6a63d43b05035e9375631bad3
bosedipayan <bosedipayan2000@gmail.com>
2024-06-29 21:58:00 +0530

    Just a modification of components

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,6 +1,6 @@
 import { Document, Schema, model, models } from "mongoose";
 
-export interface IEvento extends Document {
+export interface IEvent extends Document {
     _id: string;
     title: string;
     description?: string;
