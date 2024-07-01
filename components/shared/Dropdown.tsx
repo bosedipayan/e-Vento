71cc637248ff78009d197dbd4f3cdfcf071a73de
bosedipayan <bosedipayan2000@gmail.com>
2024-07-01 21:58:56 +0530

    Started creating and working on create event section

---
+++ components/shared/Dropdown.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type DropdownProps = {
+  value?: string;
+  onchangeHandler?: () => void;
+};
+
+const Dropdown = ({ value, onchangeHandler }: DropdownProps) => {
+  return <div>Dropdown</div>;
+};
+
+export default Dropdown;
