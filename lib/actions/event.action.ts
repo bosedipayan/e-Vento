4a0d67893358071078941ac15f5cd8e7b502ea5a
bosedipayan <bosedipayan2000@gmail.com>
2024-08-22 22:52:56 +0530

    Modified search components and fixed things accordingly

--- lib/actions/event.action.ts
+++ lib/actions/event.action.ts
@@ -70,9 +70,11 @@ export async function getAllEvents({ query, limit = 6, page, category }: GetAllE
   try {
     await connectToDatabase()
 
-    // const titleCondition = query ? { title: { $regex: query, $options: 'i' } } : {}
-    // const categoryCondition = category ? await getCategoryByName(category) : null
-    const conditions = {}
+    const titleCondition = query ? { title: { $regex: query, $options: 'i' } } : {}
+    const categoryCondition = category ? await getCategoryByName(category) : null
+    const conditions = {
+      $and: [titleCondition, categoryCondition ? { category: categoryCondition._id } : {}],
+    }
 
     const skipAmount = (Number(page) - 1) * limit
     const eventsQuery = Event.find(conditions)
