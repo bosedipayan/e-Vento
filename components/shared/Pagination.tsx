1cdda96164e15057937fdad3b6150b2b9e88d747
Dipayan Bose <bosedipayan2000@gmail.com>
2024-08-26 19:11:00 +0530

    Modification in existing parts and added pagination to it

--- components/shared/Pagination.tsx
+++ components/shared/Pagination.tsx
@@ -2,6 +2,8 @@
 
 import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
+import { Button } from "../ui/button";
+import { formUrlQuery } from "@/lib/utils";
 
 type PaginationProps = {
   urlParamName?: string;
@@ -10,9 +12,32 @@ type PaginationProps = {
 };
 
 const Pagination = ({ urlParamName, page, totalPages }: PaginationProps) => {
-  const router = useRouter;
-  const searchParams = useSearchParams;
-  return <div>Pagination</div>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const onClick = (btnType: string) => {
+    const pageValue = btnType === 'next' ? Number(page) + 1 : Number(page) - 1;
+
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: urlParamName || 'page',
+      value: pageValue.toString(),
+    })
+
+    router.push(newUrl, {scroll: false})
+  }
+
+  return (
+    <div className="flex gap-2">
+      <Button size="lg" variant="outline" className="w-28" onClick={() => onClick('prev')} disabled={Number(page) < 2}>
+        Previous
+      </Button>
+
+      <Button size="lg" variant="outline" className="w-28" onClick={() => onClick('next')} disabled={Number(page) >= totalPages}>
+        Next
+      </Button>
+    </div>
+  )
 };
 
 export default Pagination;
